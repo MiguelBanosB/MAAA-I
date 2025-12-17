c4d859017f5fcd5511d2d0db1987d0844072ff2e
MiguelBanosB <miguel.banos.baladron@udc.es>
2025-12-17 12:02:04 +0100

    commit para llevar a main

--- dependencies.jl
+++ dependencies.jl
@@ -25,8 +25,10 @@ import NearestNeighborModels  # KNN
 import LIBSVM                 # SVM
 using Flux
 import MLJFlux
-
+import DecisionTree 
 using MLJLinearModels # Necesario para LogisticClassifier
+import EvoTrees
+import MLJScikitLearnInterface
 
 # Constantes Globales
 const SEED = 104                   # Semilla fijada según el enunciado para reproducibilidad
@@ -42,3 +44,13 @@ LDA = MLJ.@load LDA pkg=MultivariateStats verbosity=0
 KNNClassifier = MLJ.@load KNNClassifier pkg=NearestNeighborModels verbosity=0
 ProbabilisticSVC = MLJ.@load ProbabilisticSVC pkg=LIBSVM verbosity=0
 NeuralNetworkClassifier = MLJ.@load NeuralNetworkClassifier pkg=MLJFlux verbosity=0
+
+# Cargar modelos de árboles
+DecisionTreeClassifier = MLJ.@load DecisionTreeClassifier pkg=DecisionTree verbosity=0
+RandomForestClassifier = MLJ.@load RandomForestClassifier pkg=DecisionTree verbosity=0
+EvoTreeClassifier = MLJ.@load EvoTreeClassifier pkg=EvoTrees verbosity=0
+
+# Para AdaBoost con SVM, necesitamos traerlos de ScikitLearn
+# Nota: "pkg=MLJScikitLearnInterface" conecta Julia con la librería de Python
+AdaBoostClassifier = MLJ.@load AdaBoostClassifier pkg=MLJScikitLearnInterface verbosity=0
+SKSGDClassifier = MLJ.@load SGDClassifier pkg=MLJScikitLearnInterface verbosity=0
